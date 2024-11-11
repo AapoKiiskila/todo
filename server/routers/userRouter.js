b8e2b630c88a7dfedab2f21c694fcdafd7b61c3f
AapoKiiskila <aapo.kiiskila@gmail.com>
2024-11-11 14:23:28 +0200

    Removed unneccessary imports

--- server/routers/userRouter.js
+++ server/routers/userRouter.js
@@ -1,9 +1,5 @@
-import { pool } from "../helpers/db.js"
 import { Router } from "express"
-import { hash,compare } from "bcrypt"
-import jwt from "jsonwebtoken"
 import { postLogin, postRegistration } from "../controllers/UserController.js"
-const { sign } = jwt
 
 const router = Router()
 
